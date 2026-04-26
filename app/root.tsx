96f19dcd59b9e6557bd9ada6a80454a082754a57
Thitiphatx <thitiphat6391@gmail.com>
2026-04-26 15:31:41 +0700

    Init

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow">
+      <div className="grow">
         <Outlet />
       </div>
       <Footer />
